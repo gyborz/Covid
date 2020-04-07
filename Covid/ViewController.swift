ae74374e01ab05af8ebcf57e540478cbbac5a21f
gyborz <borzgy95@gmail.com>
2020-04-07 11:28:16 +0200

    Initial Commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by Gyorgy Borz on 2020. 04. 07..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
